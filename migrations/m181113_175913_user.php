5b51ae80472412bfd82004251508152c8b435f36
triest <triest21@gmail.com>
2018-11-17 12:59:36 +0300

    add migrations

---
+++ migrations/m181113_175913_user.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181113_175913_user
+ */
+class m181113_175913_user extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+        $this->addColumn('user', 'fio', $this->integer()->defaultValue(null));
+        $this->addColumn('user', 'phone', $this->string()->defaultValue(null));
+        $this->addColumn('user', 'create_time',$this->dateTime()->defaultValue(null));
+    }
+
+
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+      //  $this->dropTable('user');
+        $this->dropColumn('user', 'fio');
+        $this->dropColumn('user', 'phone');
+        $this->dropColumn('user', 'create_time');
+    }
+
+  
+}
