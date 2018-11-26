7072c4cba84509f31d4a401fbe3bb80de05ff8ac
triest <triest21@gmail.com>
2018-11-26 22:23:35 +0300

    Revert "fix create review button"
    This reverts commit de2908aeee43c8e56573e9ee06fa6272f067d247.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -74,12 +74,11 @@ AppAsset::register($this);
                 <?= $content ?>
 
             </div>
-            <div class="col-xs-1 col-sm-1">
-            <?php if (Yii::$app->user->isGuest==false): ?>
-                <?= Html::a('Create review', ['/site/create'], ['class'=>'btn btn-primary']) ?>
-            <?php endif; ?>
+            <div class="list-group">
+                <?php if (Yii::$app->user->isGuest==false): ?>
+                    <?= Html::a('Создать отзыв', ['/site/create'], ['class'=>'btn btn-primary']) ?>
+                <?php endif; ?>
             </div>
-
         </div>
 
 
